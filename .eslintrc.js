6a8c261d1150345b32b3f59efacb25a6d9074a5f
lukeb1986 <lukeb29@hotmail.com>
2022-12-31 09:04:15 +1100

    done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 /**@type {import('eslint').Linter.Config} */
-eslint-disable-next-line; no-undef
+// eslint-disable-next-line no-undef
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
